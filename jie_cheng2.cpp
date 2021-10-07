9065e58032e1e18c59e417db73987c1afd38e33f
9moo <91054568+9moo@users.noreply.github.com>
2021-10-07 22:06:47 +0800

    Add files via upload

---
+++ jie_cheng2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//µÝ¹éÇó½×³Ë 
+int fac(int x)
+{
+	if(x<1)
+	return 1;
+	else
+	return x*fac(x-1);
+}
+
+int main(){
+	int a;
+	scanf("%d",&a);
+	int b = fac(a);
+	printf("½×³ËÎª%d",b);
+	return 0;
+}
